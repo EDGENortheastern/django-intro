9c201202378ce5526b567c08b5e85fb9ddd9c007
Katia <katia.punter@storyshareplatform.com>
2023-03-23 23:26:09 +0000

    Adds

--- httprequests/tests.py
+++ httprequests/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+import unittest
 
-# Create your tests here.
+class TestMyStuff(unittest.TestCase):
+
+    def test_the_obvious(self):
+        self.assertEqual(True, True)
+
+if __name__ == '__main__':
+    unittest.main()
